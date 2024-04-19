909f80bba60fc4775868a54ddeb16225ccb66fef
FarahAlmas <160884624+FarahAlmas@users.noreply.github.com>
2024-04-19 22:03:25 +0500

    Add files via upload

---
+++ 14 Ternary converstion/app.js
@@ -0,0 +1,27 @@
+"use strict";
+/*## Task 1:
+let temperature = 25;
+
+if (temperature > 30) {
+    console.log("It's too hot outside!");
+} else {
+    console.log("It's not too hot outside.");
+}*/
+//-------------------ternary operator---------------------
+let temperature = 25;
+temperature > 30 ? console.log("It's too hot outside!") :
+    console.log("It's not too hot outside.");
+/*## Task 2:
+let number = 7;
+
+if (number > 30) {
+    console.log("It's too high");
+} else if (number > 20) {
+    console.log("It's correct");
+    }else {
+    console.log("number is small");
+}*/
+let number = 7;
+number > 30 ? console.log("It's too high") :
+    number > 20 ? console.log("It's correct") :
+        console.log("number is small");
