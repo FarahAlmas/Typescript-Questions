7d9402352e7a0fc96fb6286e0d8469b01507fbab
FarahAlmas <160884624+FarahAlmas@users.noreply.github.com>
2024-04-19 21:33:49 +0500

    Add files via upload

---
+++ 6 Operators/app.ts
@@ -0,0 +1,52 @@
+//------------------------Task 1------------------------------
+
+//create two variables and add both variables. print it into terminal.
+
+let first_num = 30;
+let second_num = 20;
+let sum = first_num + second_num;
+console.log(sum);
+
+//--------------------------Task 2------------------------------
+
+//create two variables and subtract both variables. print it into terminal.
+
+let first_num1 = 30;
+let second_num1 = 20;
+let sub = first_num1 - second_num1;
+console.log(sub);
+
+//-------------------------Task 3--------------------------------
+
+//create two variables and multiply both variables. print it into terminal.
+
+let first_num2 = 30;
+let second_num2 = 20;
+let multiply = first_num2 * second_num2;
+console.log(multiply);
+
+//----------------------------Task 4-----------------------------
+
+//create two variables and divide first variable by second. print it into terminal.
+
+let first_num3 = 30;
+let second_num3 = 20;
+let divide = first_num3 / second_num3;
+console.log(divide);
+
+//-----------------------------Task 5-------------------------------
+
+//create two variables and use exponentiation. print it into terminal.
+let first_num4 = 30;
+let second_num4 = 20;
+let exponentiation = first_num4 ** second_num4;
+console.log(exponentiation);
+
+//-----------------------------Task 6--------------------------
+
+//create two variables and use modulus. print it into terminal.
+
+let first_num5 = 30;
+let second_num5 = 20;
+let modulus = first_num5 % second_num5;
+console.log(modulus);
