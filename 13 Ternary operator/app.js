08e87df663077338d164ec3bdf99630781706dc3
FarahAlmas <160884624+FarahAlmas@users.noreply.github.com>
2024-04-19 22:02:03 +0500

    Add files via upload

---
+++ 13 Ternary operator/app.js
@@ -0,0 +1,5 @@
+"use strict";
+//ternary operators
+let israiny = true;
+let use = israiny ? "wear a rain coat" : "wear a sunglass";
+console.log(`you should have ${use}`);
---
+++ 13 Ternary operator/app.ts
@@ -0,0 +1,4 @@
+//ternary operators
+let israiny = true;
+let use = israiny ? "wear a rain coat" : "wear a sunglass";
+console.log(`you should have ${use}`);
