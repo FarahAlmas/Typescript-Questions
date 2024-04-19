235e4b71f12ae81cd61083e9079de451c9ca6593
FarahAlmas <160884624+FarahAlmas@users.noreply.github.com>
2024-04-19 21:30:07 +0500

    my commit

--- 4 Code Correction/index.ts
+++ 4 Code Correction/index.ts
@@ -1,57 +1,65 @@
 // Mistake:
 leet name = "name";
-// solution:
+// solution: let Name = "name";
 
 // Mistake:
 constant number = 5;
-// solution:
+// solution: const number = 5;
 
 // Mistake:
 let MY_NAME = "name"
-// solution:
+// solution: let my_name = "name";
 
 // Mistake:
 let MYage: boolean  = "name"
-// solution:
+// solution: let myAga : number = 23;
 
 // Mistake:
 let number: num  = 6
-// solution:
+// solution: let num : number = 6;
 
 // Mistake:
 let number  = "6"
 consool.log[number]
-// solution:
+// solution: let num = 6;
+console.log(num);
 
 // Mistake:
 let father=Name: array  = "fatherName"
 log{father=Name}
-// solution:
+// solution: let fatherName :string = "fathername";
+console.log(fatherName);
 
 // Mistake:
 let father=Name: array  = "fatherName"
 log{father=Name}
-// solution:
+// solution: let fatherName :string = "fathername";
+console.log(fatherName);
 
 
 // Mistake:
 let anyNumber: number  = 65
 anyNumber = "69"
-// solution:
+// solution: let anyNum: number = 65;
+anyNum = 69;
+
 
 // Mistake:
 const myAge: number  = 26
 myAge = 60
-// solution:
+// solution: const myAge :number =26;
+const myAge2 =60;
 
 // Mistake:
 const color: string;
 const blue = "blue"
 color = `color name is ${blue}.`
-// solution:
+// solution: const color : string= "blue"
+console.log(`color name is ${color`};
 
 // Mistake:
 const color: string;
 const blue = "Black"
 color = `color name is ${"Black"}.`
-// solution:
+// solution: const color : string = "black";
+console.log(`color name is $color`};
