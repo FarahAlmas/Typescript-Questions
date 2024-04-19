220e57019e981f58515203815798e52c81b82585
FarahAlmas <160884624+FarahAlmas@users.noreply.github.com>
2024-04-19 21:07:54 +0500

    Add files via upload

---
+++ 1 Variables/app.js
@@ -0,0 +1,24 @@
+"use strict";
+//---------------------------task_1--------------------------
+Object.defineProperty(exports, "__esModule", { value: true });
+//Declare a variable with const. Its data type should be string.
+//Write your name and print it on the terminal.
+const name = "Farah Almas";
+console.log(name);
+//------------------------task_2-------------------------------------
+//Declare a variable with let. Its data type should be number. Write any number.
+//Now, change your number and print it on the terminal.
+let age = 20;
+age = 21;
+console.log(age);
+//---------------------------task_3-----------------------------------
+//Declare a variable. It should be changable. Write any number. 
+//Its data type should be number (implicitly).
+let favoriteNumber = 7;
+favoriteNumber = 8;
+console.log(favoriteNumber);
+//---------------------------task_4-----------------------------------
+//Declare a variable. It should be not be changable. Write any name. 
+//Its data type should be string (explicitly).
+const myName = "Farah Almas";
+console.log(myName);
