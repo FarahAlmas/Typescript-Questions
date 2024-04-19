f21c620eb6345a37ab008b39ce105fd76e677d0f
FarahAlmas <160884624+FarahAlmas@users.noreply.github.com>
2024-04-19 21:13:18 +0500

    Add files via upload

---
+++ 3 DataTypes/app.js
@@ -0,0 +1,57 @@
+"use strict";
+//-----------------task_1--------------------
+//Declare a variable with "srting" dataType. 
+//Add comment about your variable.
+//i put value in variable with expilicitly data type. 
+let myName = "Syed Azlan Ahmed";
+console.log(myName);
+//------------------task_2------------------------
+//Declare a variable with "Number" dataType. 
+//Add comment about your variable.
+//i put value in variable with expilicitly data type.
+let myAge = 22;
+console.log(myAge);
+//------------------task_3------------------------
+//Declare a variable with "Boolean" dataType.
+//Add comment about your variable.
+//i put value in variable with expilicitly data type.
+let isStudent = true;
+console.log(isStudent);
+//------------------task_4------------------------
+///Declare a variable with "Any" dataType. 
+//Add comment about your variable.
+//i put value in variable with expilicitly data type.
+let place = true;
+place = "Pakistan";
+place = 22;
+console.log(place);
+//------------------task_5------------------------
+//Declare a variable with "let". Its value should be "undefined". 
+//Add a comment about your variable.
+//i put value in variable with expilicitly data type.
+let myCity;
+console.log();
+//-------------------task_6---------------------------
+//Declare a variable with let. It will be of type unknown. 
+//Add a comment about your variable."
+//unknown is a special type.i put expilicitly data type.
+let myCountry;
+console.log(myCountry);
+//----------------------task_7-------------------------------
+//Declare a variable with let. It value will be bigint.
+// Add a comment about your variable."
+//bigInt 
+let a = 654766750898978;
+console.log(a);
+//--------------------task_8---------------------------
+//Declare a variable with "let". Its value will be a symbol.
+// Add a comment about your variable.
+//symbols are unique.if we put any value in our variable this symbols make unique.
+let myFriend = Symbol("Noor");
+console.log(myFriend);
+//--------------------task_9------------------------------
+//Declare a variable with "let". Its value will be "null".
+// Add a comment about your variable.
+//null is a special value.
+let myFriend2 = null;
+console.log(myFriend2);
