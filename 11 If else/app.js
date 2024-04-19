7374efff9314376381f2439dfa8ead9f7456db58
FarahAlmas <160884624+FarahAlmas@users.noreply.github.com>
2024-04-19 21:52:42 +0500

    Add files via upload

---
+++ 11 If else/app.js
@@ -0,0 +1,18 @@
+"use strict";
+// if else condition
+let result = 98;
+if (result >= 80) {
+    console.log("A+");
+}
+else if (result >= 70) {
+    console.log("A");
+}
+else if (result >= 60) {
+    console.log("B");
+}
+else if (result >= 50) {
+    console.log("C");
+}
+else {
+    console.log("Fail");
+}
---
+++ 11 If else/app.ts
@@ -0,0 +1,16 @@
+// if else condition
+let result : number = 98;
+
+
+if (result >= 80) {
+    console.log("A+");
+}else if (result >= 70) {
+    console.log("A");
+}else if (result >= 60) {
+    console.log("B");
+}else if (result >= 50) {
+    console.log("C");
+}else{
+    console.log("Fail");
+}
+
