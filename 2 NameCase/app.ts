2f8a9f62f695563ae0bb5d83b46d98c6df858b84
FarahAlmas <160884624+FarahAlmas@users.noreply.github.com>
2024-04-19 21:10:17 +0500

    Add files via upload

---
+++ 2 NameCase/app.ts
@@ -0,0 +1,21 @@
+//--------------------------task_1-------------------------------------
+
+//Declare a variable. Its name-case should be camel-case. Its data type should be string. 
+//Write your name and print it on the terminal.
+let myName: string = "Farah Almas";
+console.log(myName); 
+
+
+//---------------------------task_2---------------------------------------
+
+//Declare a variable. Its name-case should be snake-case. Its data type should be string. 
+//Write your name and print it on the terminal.
+let my_name: string = "Farah Almas";
+console.log(my_name);
+
+//--------------------------task_3----------------------------------------
+
+//Declare a variable. Its name-case should be pascal-case. Its data type should be string. 
+//Write your name and print it on the terminal.
+let MyName: string = "Farah Almas";
+console.log(MyName);
